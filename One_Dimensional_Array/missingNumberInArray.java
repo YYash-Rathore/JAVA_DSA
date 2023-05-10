e71e3d756a483bfa35a7ca0f3b383f615eff2307
YYash-Rathore <yashrathore5dec@gmail.com>
2023-05-10 13:25:40 +0530

    Missing Number in an Array

---
+++ One_Dimensional_Array/missingNumberInArray.java
@@ -0,0 +1,14 @@
+public class missingNumberInArray {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,7,6,5};
+        // Expected Output: 3
+        int n = arr.length+1;
+        int sumOfNaturalNo = (n*(n+1))/2;
+        int sumOfArr = 0;
+        for(int i = 0; i<arr.length;i++){
+            sumOfArr += arr[i];
+        }
+        int missingNumber = sumOfNaturalNo - sumOfArr;
+        System.out.println("Missing Number is: "+ missingNumber);
+    }
+}
