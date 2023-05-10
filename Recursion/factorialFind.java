d0f1bfd519de8ed491bb0f7aa616b01c6fe092dd
YYash-Rathore <yashrathore5dec@gmail.com>
2023-05-10 22:06:17 +0530

    Recursion final

--- Recursion/factorialFind.java
+++ Recursion/factorialFind.java
@@ -1,22 +1,28 @@
 package Recursion;
 
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class factorialFind {
+class factorialFind {
+    // function definition
     public static int factorialFinding(int n){
         int result = 0;
-        if(n==0 || n==1){
+        // Base case condition
+        if(n == 0 || n == 1){
             return 1;
         }
+        // Recursive calls
         else{
-            result = n*factorialFinding(n-1);
+            result = n * factorialFinding(n-1);
         }
         return result;
     }
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number whose factorial you want to find out: ");
         int n = sc.nextInt();
-        int factorial = factorialFinding(n);
-        System.out.println("The Factorial is: "+factorial);
-    }    
+        // function calling
+        int result = factorialFinding(n);
+        System.out.println("Factorial of a given number entered by the user is: "+ result);
+    }
 }
