5c776b78cf6d8529089e03d76d0ca0731b8dda12
YYash-Rathore <yashrathore5dec@gmail.com>
2023-07-17 16:23:44 +0530

    linked list

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -34,6 +34,30 @@ class LinkedList {
         return;
     }
 
+    // Insertion of the newNode at the very begining in the Linked List
+    public void insertAtBegining(int newData){
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insertion of a newNode After any Node in a Linked List
+    public void insertAfterNode(Node prev_node,int newData){
+        if(prev_node == null){
+            System.out.println("Please Enter a Valid Node as it contains null value");
+        }
+
+        Node newNode = new Node(newData);
+        newNode.next = prev_node.next;
+        prev_node.next = newNode;
+        // Node temp = head;
+        // while(temp.data != data){
+        //     temp = temp.next;
+        // }
+        // newNode.next = temp.next;
+        // temp.next = newNode;
+    }
+
     // display all the nodes after the insertion
     public void printNodes(){
         Node current = head;
@@ -53,11 +77,18 @@ class LinkedList {
         llist.insertAtEnd(8);
         llist.insertAtEnd(10);
         llist.insertAtEnd(15);
-
+        llist.insertAtEnd(19);
         System.out.println("Linked List after successful insertion of all the nodes:");
         llist.printNodes();
         System.out.println();
 
+        llist.insertAtBegining(10);
+        System.out.println("Linked List after successful insertion of a node at begining");
+        llist.printNodes();
+        System.out.println();
 
+        llist.insertAfterNode(llist.head.next.next, 9);
+        System.out.println("Linked List after successful insertion after any node");
+        llist.printNodes();
     }
 }
